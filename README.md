ff10e14fa58f08dfbe5ddfb5cc49437efa9d2e83
Pedro Martins <snitramordep@gmail.com>
2022-10-06 13:04:01 +0100

    Change project structure. Add new modal type (confirm)

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-## Modal
+## MODAL - Javascript Module
+----------------------------
+
+Simple javascript module to create modal windows. 
+It can be used to generate diferent types of modals: alert, error, confirm, image or just plain content with a few lines of code.
+
+## Setup
+- Add modal.css in your `<head>`.
+- Add slick.js before your closing `<body>` tag.
+
+## Usage
+Initialize the modal in your script file or an inline script tag.
+
+    let text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. lorem ipsum dolor sit amet consectetur adipisicing elit.';
+
+    modal.show({
+        body: text
+    });
+
+
+## Settings
+- `body` (*type String*)
+    - *Required*. 
+    - Text to be displayed in the modal body.
+- `btnTxt` (*type String*)
+    - *Optional*. 
+    - Text to be displayed in the modal button.
+- `position` (*type String*)
+    - *Optional*. 
+    - Modal position: `'default'` (by default) or `'centered'`.
+- `size` (*type String*)
+    - *Optional*. 
+    - Sets modal sizes: `'m'` (by default), `'s'` and `'l'`.
+- `title` (*type String*)
+    - *Optional*. 
+    - Text to be displayed in the modal title.
+- `type` (*type String*)
+    - *Optional*. 
+    - Types of Modal: `'content'` (by default), `'error'`, `'warning'`, `'image'`.
+
+## Public methods
+- To initialize the modal
+
+    ```modal.show();```
+- To remove the modal. Should be usend inside a callback function like the one passed on `defaults.cbFn`
+
+    ```modal.hide();```
--- src/modal.js
+++ src/modal.js
@@ -1,21 +1,29 @@
+/**
+ * modal.js
+ * 
+ * author: Pedro Martins
+ * url: https://github.com/ptmartins | https://ptmartins.github.io
+ * 
+ */
+
 (function(modal) {
     // Do modal stuff here
 
     let DOM = {
 
         },
-
-        modifiers = '';
     
         /**
          * Modal default settings
          */
         defaults = {
-            type: 'content', // content, confirm, alert, error, image
+            body: '',
+            btnTxt: '',
+            cbFn: null,
+            position: 'default', // default, centered,
             size: 'm', // s, m, l
             title: '',
-            body: '',
-            btnTxt: ''
+            type: 'content', // content, confirm, warning, error, image
         },
 
         /**
@@ -38,7 +46,7 @@
             error: () => `
                 <div class="modal__header modal__header--error">
                     <h2 class="modal__title">
-                        <span class="modal__icon modal__icon--error"><span>
+                        <span class="modal__icon modal__icon--error">&#10006;</span>
                         ${defaults.title ? defaults.title : 'Error'}
                     </h2>
                     <button class="modal__close"></button>
@@ -52,10 +60,10 @@
                     </button>
                 </div>
             `,
-            alert: () => `
+            warning: () => `
                 <div class="modal__header modal__header--alert">
                     <h2 class="modal__title">
-                        <span class="modal__icon modal__icon--alert"><span>
+                        <span class="modal__icon modal__icon--alert">&#33;</span>
                         ${defaults.title ? defaults.title : 'Alert'}
                     </h2>
                     <button class="modal__close"></button>
@@ -65,10 +73,25 @@
                 </div>
                 <div class="modal__footer">
                     <button class="modal__btn modal__btn--close">
-                        ${defaults.btnTxt ? defaults.btnTxt : 'Ok'}
+                        ${defaults.btnTxt ? defaults.btnTxt : 'Acknowledge'}
                     </button>
                 </div>
-            `
+            `,
+            confirm: () => `
+                <div class="modal__header modal__header--confirm">
+                    <h2 class="modal__title">
+                        ${defaults.title ? defaults.title : ''}
+                    </h2>
+                    <button class="modal__close"></button>
+                </div>
+                <div class="modal__body">
+                    ${defaults.body}
+                </div>
+                <div class="modal__footer">
+                    <button class="modal__btn modal__btn--confirm">${defaults.btnTxt ? defaults.btnTxt : 'Confirm'}</button>
+                    <button class="modal__btn modal__btn--close">Close</button>
+                </div>
+         `
         }
 
         /**
@@ -79,7 +102,7 @@
             DOM.modal = document.createElement('DIV');
 
             DOM.modalOverlay.className = 'modal__overlay';
-            DOM.modal.className = (`modal modal--${defaults.type} modal--${defaults.size}`);
+            DOM.modal.className = (`modal modal--${defaults.type} modal--${defaults.size} modal--${defaults.position}`);
 
             switch(defaults.type) {
                 case 'content':
@@ -92,14 +115,20 @@
                 case 'error':
                     DOM.modal.innerHTML = modalMarkup.error();
                     break;
-                case 'alert':
-                    DOM.modal.innerHTML = modalMarkup.alert();
+                case 'warning':
+                    DOM.modal.innerHTML = modalMarkup.warning();
+                    break;
+                case 'confirm':
+                    DOM.modal.innerHTML = modalMarkup.confirm();
                     break;
             };
 
             document.body.append(DOM.modalOverlay, DOM.modal);
             DOM.closeBtn = document.querySelector('.modal__close');
             DOM.cancelBtn = document.querySelector('.modal__btn--close');
+            if(defaults.type === 'confirm') {
+                DOM.confirmBtn = document.querySelector('.modal__btn--confirm');
+            }
         },
 
         /**
@@ -109,6 +138,10 @@
             DOM.closeBtn.addEventListener('click', hide);
             if(DOM.cancelBtn) {
                 DOM.cancelBtn.addEventListener('click', hide);
+            }
+
+            if(defaults.cbFn && defaults.type === 'confirm') {
+                DOM.confirmBtn.addEventListener('click', defaults.cbFn);
             }
         },
 
